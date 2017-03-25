978a9087e1471b2edc57b10712bc9c97bc3c2d7e
yueqian <yueqian@meili-inc.com>
2017-03-25 18:26:41 +0800

    adapter

---
+++ app/src/main/java/com/kaneki/done/adapter/DoneListAdapter.java
@@ -0,0 +1,33 @@
+package com.kaneki.done.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.kaneki.done.model.holder.DoneListHolder;
+
+/**
+ * @author yueqian
+ * @Desctription
+ * @date 2017/3/25
+ * @email yueqian@mogujie.com
+ */
+public class DoneListAdapter extends RecyclerView.Adapter<DoneListHolder> {
+
+    public DoneListAdapter() {
+    }
+
+    @Override
+    public DoneListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(DoneListHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/kaneki/done/model/holder/DoneListHolder.java
@@ -0,0 +1,16 @@
+package com.kaneki.done.model.holder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * @author yueqian
+ * @Desctription
+ * @date 2017/3/25
+ * @email yueqian@mogujie.com
+ */
+public class DoneListHolder extends RecyclerView.ViewHolder {
+    public DoneListHolder(View itemView) {
+        super(itemView);
+    }
+}
